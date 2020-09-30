851d28324fefbb572625d3f6f1f6ad2cff66597f
murylocesar <42966369+murylocesar@users.noreply.github.com>
2020-09-29 22:22:05 -0300

    Criação de rotas e controller para agendamento

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 
 import AppError from '@shared/errors/AppError';
 import { injectable, inject } from 'tsyringe';
@@ -26,23 +26,48 @@ class CreateAppointmentService {
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    // const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
-    //   appointmentDate,
-    //   provider_id,
-    // );
-    // if (user_id === provider_id) {
-    //   throw new AppError("You can't create an appointment with yourself.");
-    // }
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You can't create an appointemnt on a past date.");
+    }
 
-    // if (findAppointmentInSameDate) {
-    //   throw new AppError('This appointment is already booked', 400);
-    // }
+    if (user_id === provider_id) {
+      throw new AppError("You can't create an appointment with yourself.");
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError(
+        'You can only create appointments between 8am and 5pm',
+      );
+    }
+
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
+      appointmentDate,
+      provider_id,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new AppError('This appointment is already booked');
+    }
 
     const appointment = await this.appointmentsRepository.create({
       provider_id,
       user_id,
       date: appointmentDate,
     });
+
+    // const dateFormatted = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm'h'");
+
+    // await this.notificationsRepository.create({
+    //   recipient_id: provider_id,
+    //   content: `Novo agendamento para dia ${dateFormatted}`,
+    // });
+
+    // await this.cacheProvider.invalidate(
+    //   `provider-appointments:${provider_id}:${format(
+    //     appointmentDate,
+    //     'yyyy-M-d',
+    //   )}`,
+    // );
 
     return appointment;
   }
